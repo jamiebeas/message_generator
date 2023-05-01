29fea654a849e5b6790a89076b255c86f3a32350
Jamiebeas <info@jamiebeas.com>
2023-05-01 15:44:01 +0800

    Final commit. Code complete

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-// Random workout generator
+// Randomly generate a bodyweight workout by calling generateWorkout()
 
-const movements = ['squat', 'deadlift', 'benchPress', 'pullups', 'overheadPress', 'sitUps'];
-    console.log(movements);
+const exercises = ['burpees', 'push-ups', 'Air squats', 'lunges', 'sit-ups', 'pull-ups'];
+const reps = [10, 15, 20, 25, 30];
+const rounds = [2, 3, 4, 5, 6];
+
+// Fisher-Yates shuffle algorithm to shuffle array in place
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const generateWorkout = () => {
+  const numRounds = rounds[Math.floor(Math.random() * rounds.length)];
+  console.log(`Complete ${numRounds} rounds: of`);
+
+  const shuffledExercises = shuffleArray([...exercises]);
+  for (let i = 1; i <= numRounds; i++) {
+    const exercise = shuffledExercises[i - 1];
+    const rep = reps[Math.floor(Math.random() * reps.length)];
+    console.log(`- ${rep} ${exercise}`);
+  }
+};
+
+generateWorkout(); // Call the function to generate your random workout
